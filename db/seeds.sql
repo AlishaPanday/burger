dc51f056aa9692be4b5de7fc9d616f28ac57eefe
Alisha Panday <panday.alisha90@gmail.com>
2021-02-24 17:36:01 +0800

    Create burger schema and inserted value in it

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('big mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Burger', false);
