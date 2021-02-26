02113ab4b7a816fa0645d90c49bd2d198b632c3c
Alisha Panday <panday.alisha90@gmail.com>
2021-02-26 19:39:45 +0800

    conenction.js updated

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,18 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burger_db',
-});
+let connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+  connection = mysql.createConenction(process.env.JAWSDB_URL)
+}
+else {
   connection = mysql.createConnection({
-    host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: 'localhost',
     port: 3306,
-    user: "oivbg2qpjfaeh5x6",
-    password: "hn9d9fm5cjvljlec",
-    database: "gd2y01knsble1yq9",
+    user: 'root',
+    password: 'root',
+    database: 'burger_db',
   });
 }
 
