909749415573abbb76b14ddd63b89e9c5a3d7399
Alisha Panday <panday.alisha90@gmail.com>
2021-02-26 18:17:56 +0800

    created connection for jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burger_db',
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConenction(process.env.JAWSDB_URL)
+}
+else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'burger_db',
+  });
+}
+
 
 // Make connection.
 connection.connect((err) => {
