35a981cb23baccb1b1d6d18df723620cc20c14fb
Alisha Panday <panday.alisha90@gmail.com>
2021-02-27 11:35:38 +0800

    removed gif image

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,7 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-// let connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConenction(process.env.JAWSDB_URL)
-// }
-// else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: 'root',
-//     database: 'burger_db',
-//   });
-// }
 const dsn = "mysql://oivbg2qpjfaeh5x6:hn9d9fm5cjvljlec@l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/gd2y01knsble1yq9"
 const connection = mysql.createConnection({
   host:"l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
